907d51392cf550c223a66b10059fde9da1f68506
test-acc <166982942+noelcpp@users.noreply.github.com>
2024-10-09 10:58:15 +0000

    Day3

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print ("Codecrafters")
+print("Hello World")
+name: str = input("Enter yout name: ")
+print(f"The name is {name}") 
+
+number_1: int = float(input("enter a number 1: "))
+number_2: int = float(input("enter the number 2: "))
+
+if number_1==number_2:
+    print("both are same")
+
+elif number_1>number_2:
+    print(f"{number_1} is greater than{number_2}") 
+elif number_1<number_2:
+    print(f"{number_1} is greater than {number_2}")     

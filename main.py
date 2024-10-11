9d3b28872ae4270e1918575757514679d9b70606
test-acc <166982942+noelcpp@users.noreply.github.com>
2024-10-11 04:49:55 +0000

    first commit on 11/10

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-print("Hello World")
-name: str = input("Enter yout name: ")
-print(f"The name is {name}") 
+#inuput
 
-number_1: int = float(input("enter a number 1: "))
-number_2: int = float(input("enter the number 2: "))
+string_input: 
 
-if number_1==number_2:
-    print("both are same")
+#data
 
-elif number_1>number_2:
-    print(f"{number_1} is greater than{number_2}") 
-elif number_1<number_2:
-    print(f"{number_1} is greater than {number_2}")     
+Art = """
+Artificial intelligence (AI) focuses on creating machines and software that can perform tasks typically requiring human intelligence. This includes capabilities such as learning from experience, understanding natural language, recognizing patterns, and making decisions. AI technologies, like machine learning and neural networks, are increasingly integrated into various applications, from virtual assistants and chatbots to advanced robotics and autonomous vehicles. The ongoing research and development in AI promise to enhance efficiency and productivity across multiple industries, while also raising important ethical considerations regarding bias, transparency, and the impact on employment.
+"""
+
+cyb = """
+Cybersecurity is the practice of protecting computer systems, networks, and data from theft, damage, or unauthorized access. With the rise of digital technologies, the importance of cybersecurity has skyrocketed, as organizations face increasing threats from cybercriminals and malicious actors. Effective cybersecurity measures encompass a range of strategies, including encryption, firewalls, intrusion detection systems, and user education. As cyber threats evolve, so too must security protocols, emphasizing the need for continuous monitoring and updating of defenses to safeguard sensitive information and maintain trust in digital platforms.
+"""
+
+clo = """
+Cloud computing refers to the delivery of computing services—such as storage, processing power, and applications—over the internet, allowing users to access resources on demand. This model offers flexibility, scalability, and cost efficiency, making it popular among businesses and individuals alike. Major cloud service providers, such as Amazon Web Services (AWS), Microsoft Azure, and Google Cloud, enable organizations to host applications, store data, and utilize advanced computing capabilities without the need for extensive physical infrastructure. As cloud computing continues to evolve, it fosters innovation by facilitating collaboration, remote work, and the development of new technologies.
+"""
+
+#end_data
